c267ab97b3d17f572169bb5ae49f0b09875a6830
TewatiaAryan <95906032+TewatiaAryan@users.noreply.github.com>
2022-04-03 17:08:16 +0530

    Create insertion_sort.java

---
+++ insertion_sort.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class insertion_sort{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int[] arr=new int[sc.nextInt()];
+        System.out.print("Enter the elements of array : ");
+        for(int i=0;i<arr.length;i++)
+            arr[i]=sc.nextInt();
+        for(int i=1;i<arr.length;i++){
+            int current=arr[i];
+            int j=i-1;
+            while(j>=0 && current<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+        }
+        for(int i=0;i<arr.length;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
